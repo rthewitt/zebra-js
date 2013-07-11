054e74ba1b521426c76dfc654f37b5b8736a3960
Ryan Hewitt <rthewitt.devel@gmail.com>
2013-07-10 19:50:23 -0700

    changing direction, using graph.  Global constraints were inefficient subfilter

--- zebra.js
+++ zebra.js
@@ -33,8 +33,10 @@ function getConstraint(scope, rel, optional) {
         return function(){return rel.apply(this, args)}
     }
     var args = optional || [];
-    c.force = f(this.scope.concat(args)); // in case unary/nary with arguments
-    return c;
+    var constraint = new c();
+    constraint.scope = constraint.scope.concat(scope);
+    constraint.force = f(scope.concat(args)); // in case unary/nary with arguments
+    return constraint;
 }
 
 function isAssigned(dom) {
@@ -61,6 +63,14 @@ function getAssigned(vars) {
         return a === val;
     }
 
+    // TEMPORARY
+    var RIGHT = function(a, b) {
+        return b-a === 1;
+    }
+    var NEXT = function(a, b) {
+        return Math.abs(b-a) === 1;
+    }
+
     // vars is an array of domain arrays!! 
     var ALL_DIFF = function(vars) {
         var modified = false;
@@ -71,22 +81,99 @@ function getAssigned(vars) {
             vars = _.chain(vars)
                 .reject(isAssigned) // remove them from original
                 .map(function(vDomain){
-                    return _.difference(vDomain, assigned);
+                    console.log("FILTERING: modified was: "+modified); // verify closure behavior
+                    var filtered = _.difference(vDomain, assigned);
+                    if(filtered.length !== vDomain.length)
+                        modified = true; 
+                    return filtered;
                 }).value();
 
             assigned = getAssigned(vars); // check again for singletons
         }
+        return modified;
     }
+
+//#############################################
+////////// ALT. CONSISTENCY ALGORITHMS  //////
+//#############################################
+
+function ac3(csp, assignment) {
+    var Q = [];
+    for(var vertex in csp.graph)
+        for(var affected in csp.graph[vertex].neighbors)
+            Q.unshift({v1: vertex, v2: affected});
+
+    while(Q.length > 0) {
+        var arc = Q.shift();
+        if(revise()) {
+            if(assignment[arc.v1].length === 0)
+                return false;
+            var otherNeighbors = _(csp.graph[vertex].neighbors).without(v2);
+            for(var possiblyAffected in otherNeighbors) 
+                Q.unshift({v1: possiblyAffected, v2: vertex});
+        }
+    }
+}
+
+// TODO change back from assignment
+function revise(csp, assignment, v1, v2) {
+    var modified = false;
+    var found=false;
+    // use constraint whose scope is below
+    // if no value for v2 exists in C(v1,v2), remove v1 from domaim
+    for(var v1Val in assignment[v1]) {
+        for(var value in assignment[v2]) {
+            
+        }
+    }
+}
+
+/* these variables have not been ported
+function revise(a, b) {
+    var deleted = false;
+    _.each(a.domain, function(value){
+        var found = false;
+        _each(value, );
+    });
+}
+*/
+
+function getMostConstrainedVar(assignment) { // FIXME just grabs first
+    var ret=null;
+    for(var variable in assignment) {
+        if(assignment[variable].length > 1)
+            ret = assignment[variable];
+    }
+    return ret;
+}
 
 
 //#############################################
 ////////// PROBLEM DEFINITION ////////////////
 //#############################################
   
+var csp = function(variables, domain, constraints) {
+    this.domain = domain;
+    this.variables = variables;
+    this.constraints = [];
+    var self = this;
+    _.each(constraints, function(cc){
+        self.constraints.push(getConstraint.apply(self, cc));
+    });
+    this.buildConstraintGraph();
+
+    console.log('checking graph\n')
+    for(var nn in this.graph) {
+        console.log('\n'+nn+': '+this.graph[nn].neighbors);
+    }
+    // sort variables by inspecting constraints
+    // for each constraint, for each in scope push unique others in scope
+    //this.neighbors = _.map();
+}
 
 csp.prototype.solve = function() {
     var assignment = {}; 
-    _.each(this.variables, function(name){assignment[name]=domain.splice();});
+    _.each(this.variables, function(name){assignment[name]=domain.slice();});
     var solution = this.backTrack(assignment);
     if(solution !== FAILURE && solution instanceof Array)
         alert("Solved!\n"+solution);
@@ -97,28 +184,57 @@ csp.prototype.isComplete = function(assignment) {
     return getAssigned().length === this.variables.length;
 }
 
-csp.prototype.backTrack(assignment) {
+csp.prototype.backTrack = function(assignment) {
+    console.log(assignment);
     if(this.isComplete(assignment)) return assignment;
 
-    var considered = getMostConstrainedVar(); // FIXME
+    var considered = getMostConstrainedVar(assignment); // FIXME
+
+    console.log('considering '+considered);
+    if(considered === null) {
+        console.error("Unable to find constrained var, verify state.");
+        return FAILURE;
+    }
 
     var snapshot = considered;
     for(var val in this.domain) {
         if(val in considered){
             var inference = {};
-            assignment[considered.name] = _reject();
+            //assignment[considered.name] = _reject();
 
             // INFERENCE
+            this.infer();
             // END INFERENCE
 
         }
         // completely revert
-        assignment.frontier[vrb] = snapshot; // FIXME this does not revert inferences!!!!
+        //assignment[considered] = snapshot; // FIXME this does not revert inferences!!!!
     }
     return FAILURE;
 }
 
+csp.prototype.infer = function() {
+    // CHOOSE APPROPRIATE ALGORITHM BASED ON
+    // TYPES OF CONSTRAINTS.  POSSIBLE HYBRID SELECTION?
+}
 
+csp.prototype.buildConstraintGraph = function() {
+    this.graph = this.graph || {};
+    var self = this;
+    console.log('Number of constraints: '+self.constraints.length);
+    _.each(self.constraints, function(cc){
+        console.log('Constraint of length: '+cc.scope.length);
+        for(var i=0; i<cc.scope.length; i++) {
+            var vertex = cc.scope[i];
+            var others = _.without(cc.scope, vertex);
+            console.log('scope check '+others);
+            if(vertex in self.graph)
+                self.graph[vertex].neighbors = _(self.graph[vertex].neighbors.concat()).uniq(); // mixed style
+            else 
+                self.graph[vertex].neighbors = others;
+        }
+    });
+}
 
 var constraints = [
     [ ["englishman", "red"], SAME ],
@@ -136,17 +252,7 @@ var constraints = [
     [ ["coffee", "green"], SAME ],
     [ ["milk"], VALUE, 2]
 ];
-var problem = csp(domains, constraints);
 
-var csp = function(variables, domain, constraints) {
-    this.domain = domain;
-    this.variables = variables;
-    this.constraints = [];
-    _.each(constraints, function(cc){
-        this.constraints.push(getConstraint.apply(this, cc);
-    });
-    // sort variables by inspecting constraints
-    // for each constraint, for each in scope push unique others in scope
-    //this.neighbors = _.map();
-}
 
+var problem = new csp(variables, domain, constraints);
+problem.solve();
